7a658d86e89a2576967c89b17867710f05302f4a
jugrajn <jugraj.nijjar@gmail.com>
2021-05-20 18:00:41 -0400

    assigned prompts

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Assign prompts
+var lengthPrompt = "How long do you want your password? (Must choose between 8 & 128 characters)"
+var lowerPrompt = "Do you want lowercase?"
+var upperPrompt = "Do you want uppercase?"
+var numericPrompt = "Do you want numeric?"
+var specialPrompt = "Do you want special characters?"
+var isNumber = (8 < x > 128)
+
 // Write password to the #password input
 function writePassword() {
+  prompt(lengthPrompt);
+  if (isNumber === True) {
+    prompt()
+  }
+
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
